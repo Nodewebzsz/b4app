5e281b63a49c6ea2de94213b537823eb13b98997
Nodewebzsz <zszxingchenid@gmail.com>
2023-08-16 21:31:06 +0800

    Update entrypoint.sh
    nezha

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,7 @@ sed -i "s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/ngin
 
 # 请删掉下一行最前面的 # 号，同时将这行代码中的域名替换成 Replit 为你项目分配的域名。
 #while true ;do curl https://xx.xx.repl.co; sleep 300; done &
+# 如果有设置哪吒探针三个变量,会安装。如果不填或者不全,则不会安装
+[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && echo '0' | ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY}
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
